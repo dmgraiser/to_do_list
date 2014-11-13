b38474813f4f535d04361830d945d82721a3d7ed
Doug Graiser <dmgraiser@gmail.com>
2014-11-13 14:10:13 -0500

    Round 2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.all.order(:dueby)
   end
 
   # GET /items/1
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+  	@items = Item.all.order(:dueby)
+
+			@date = []
+    	@date.push(:dueby)
+		  @date
+	    	
+  	
+    # if item.dueby != ""
+    # 	@show_date = "#{@items.dueby}"
+    # end
+
   end
 end
